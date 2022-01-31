ebf2177202048a4bc02c860fb74bda322bd93cda
EricR19 <eric19rr@outlook.es>
2022-01-31 12:19:29 -0600

    Project

---
+++ portfolio/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main-content">
+      <div className="up-content">
+        <div className="logo">
+          {" "}
+          LOGO
+          <img className="img"></img>
+        </div>
+        <div className="projects">PROJECTS</div>
+        <div className="contact">CONTACT</div>
+      </div>
+      <div className="mid-content">
+        <div className="name">
+          {"ERIC"}
+          <div className="a">A Junior</div>
+          <div className="b">Front-End Developer</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
